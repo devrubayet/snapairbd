48649d5b15924d7feb65cb2169871de9d5f303e7
rubu <rubayethossain2003@gmail.com>
2026-08-07 23:46:02 +0600

    update showcase img

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -144,7 +144,7 @@
 
             <div class="offer-add mt-8 mx-3 p-7">
                 <div class="ads-img w-full h-32 rounded-md overflow-hidden">
-                    <img class="w-full h-full object-f" src="img/add.png" alt="" />
+                    <img class="w-full h-full object-f" src="img/showcase" alt="" />
                 </div>
             </div>
         </div>
